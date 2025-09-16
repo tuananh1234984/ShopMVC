29a8b2f8f38c54bb74fd3350662254295dcbb457
tuananh123984 <166016866+tuananh1234984@users.noreply.github.com>
2025-09-16 22:16:23 +0700

    Initial commit: ASP.NET Core MVC project (Razor Views), container settings aligned to Linux

---
+++ Web/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("shop.html")]
+        [HttpGet("shop")]
+        public IActionResult Shop()
+        {
+            return View(); // Will resolve to Views/Product/Shop.cshtml
+        }
+    }
+}
